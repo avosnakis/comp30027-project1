6ece9108ee477eeddda7c849349a9779eb933bc8
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-10 10:51:03 +1100

    preprocess now seperates training data and testing data

--- src/preprocess.py
+++ src/preprocess.py
@@ -1,11 +1,27 @@
 import csv
 
-from typing import List
+from typing import List, Tuple
 
-def preprocess(filename: str) -> List[List[str]]:
-    data: List[List[str]] = list()
+TRAINING_FRACTION = .8
+
+def preprocess(filename: str) -> Tuple[List[List[str]], List[List[str]]]:
+    training_data: List[List[str]] = list()
+    testing_data: List[List[str]] = list()
+    num_instances = _get_num_instances(filename)
+    num_training_instances: int = round(TRAINING_FRACTION * num_instances)
+
     with open(filename) as csvfile:
+        i: int = 0
         for row in csv.reader(csvfile):
-            data.append(row)
+            if i < num_training_instances:
+                training_data.append(row)
+            else:
+                testing_data.append(row)
+            i += 1
+
     csvfile.close()
-    return data
+    return (training_data, testing_data)
+
+def _get_num_instances(filename : str) -> int:
+    with open(filename) as csvfile:
+        return sum(1 for row in csvfile)
