30a704ed9fc3604bacf3d5040716756568fb8159
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-09 16:56:45 +1100

    started valuematrix class

---
+++ src/ValueMatrix.py
@@ -0,0 +1,12 @@
+"""
+Wrapper class for the value matrix
+"""
+
+import csv
+
+class ValueMatrix:
+    def __init__(self):
+        self.__struct: list = list()
+
+    def add_row(self, row):
+        pass
