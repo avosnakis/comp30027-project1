5aabedb10bb049885af5c709aeacc132cc4e8d78
ngweihow <ngweihow.r@gmail.com>
2018-03-10 11:34:34 +1100

    first commit for unsupervised

--- src/ValueMatrix.py
+++ src/ValueMatrix.py
@@ -4,6 +4,7 @@ Wrapper class for the value matrix
 
 from typing import List, DefaultDict, Dict
 from collections import defaultdict
+from random import random
 
 EMPTY_CELL: str = "?"
 CLASS_CELL: int = -1
@@ -26,6 +27,13 @@ class ValueMatrix:
         for i in range(len(row) - 1):
             self.__incr_cell(instance_class, row[i], i)
 
+
+    def label_unsupervised(self, ):
+        """
+        Generic Method which labels the unsupervised data rows with a
+        non-uniformly distributed probability of each class 
+        """
+
     def __new_dict(self) -> None:
         """
         Creates a new defaultdict for this class, and a cell mapping for it
@@ -34,7 +42,7 @@ class ValueMatrix:
 
     def __incr_cell(self, instance_class: str, attr: str, curr_cell: int) -> None:
         if attr == EMPTY_CELL:
-            return        
+            return
         self.__struct[curr_cell][instance_class][attr] += 1
 
     def print_matrix(self):
