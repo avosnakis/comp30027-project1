b746a20e9a0e9b49e41625453bca961dc9043498
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-12 12:39:48 +1100

    hotfix: changed functions to use testing data

--- src/evaluate_supervised.py
+++ src/evaluate_supervised.py
@@ -3,5 +3,5 @@ from SupervisedNBClassifier import SupervisedNBClassifier
 
 
 def evaluate_supervised(classifier: SupervisedNBClassifier, data: ClassifierData) -> None:
-    percentage_correct: float = classifier.evaluate(data.get_training_data())
+    percentage_correct: float = classifier.evaluate(data.get_testing_data())
     print("Percentage of correct classifications: {:.6}%".format(percentage_correct))
--- src/predict_supervised.py
+++ src/predict_supervised.py
@@ -5,6 +5,6 @@ from ClassifierData import ClassifierData
 
 
 def predict_supervised(classifier: SupervisedNBClassifier, data: ClassifierData) -> None:
-    predicted: List[Tuple[str, List[str]]] = classifier.predict_set(data.get_training_data())
+    predicted: List[Tuple[str, List[str]]] = classifier.predict_set(data.get_testing_data())
     for pred in predicted:
         print("Predicted class: {}, instance: {}".format(pred[0], pred[1]))
