82af47d4e9e82a64ffc19256bccf38b16e9d923e
Alex Vosnakis <alex.vosnakis@alexsolutions.com.au>
2018-03-17 14:01:55 +1100

    Moved all supervised-specific functions in the supervised.py module, added some docs

---
+++ src/supervised.py
@@ -0,0 +1,41 @@
+"""
+Module for all functions driving the supervised classifier.
+"""
+
+from typing import List, Tuple
+
+from ClassifierData import ClassifierData
+from SupervisedNBClassifier import SupervisedNBClassifier
+
+
+def train_supervised(data: ClassifierData) -> SupervisedNBClassifier:
+    """
+    Instantiates a new supervised Naive Bayes Classifier and trains it.
+    :param data: The data to be fed into the classifier.
+    :return: A trained supervised NB classifier.
+    """
+    classifier: SupervisedNBClassifier = SupervisedNBClassifier()
+    classifier.train(data)
+    return classifier
+
+
+def predict_supervised(classifier: SupervisedNBClassifier, data: List[List[str]]) -> None:
+    """
+    Predicts the classes for a set of instances.
+    :param classifier: The trained classifier.
+    :param data: The instances to classify.
+    """
+    predicted: List[Tuple[str, List[str]]] = classifier.predict_set(data)
+    for pred in predicted:
+        print("Predicted class: {}, instance: {}".format(pred[0], pred[1]))
+
+
+def evaluate_supervised(classifier: SupervisedNBClassifier, data: ClassifierData) -> None:
+    """
+    Evaluates a classifier against a set of data, printing out the percentage of correct
+    classifications.
+    :param classifier: The trained classifier.
+    :param data: The data holing the testing data.
+    """
+    percentage_correct: float = classifier.evaluate(data.get_testing_data())
+    print("Percentage of correct classifications: {:.6}%".format(percentage_correct))
--- src/train_supervised.py
+++
@@ -1,8 +0,0 @@
-from ClassifierData import ClassifierData
-from SupervisedNBClassifier import SupervisedNBClassifier
-
-
-def train_supervised(data: ClassifierData) -> SupervisedNBClassifier:
-    matrix: SupervisedNBClassifier = SupervisedNBClassifier()
-    matrix.train(data)
-    return matrix
